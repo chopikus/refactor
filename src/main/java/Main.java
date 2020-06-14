d5a8d35143f146c5a2bb7dc5b88310dd93ea2219
ihor <cigo1234r@gmail.com>
2020-06-14 13:12:48 +0300

    Добавлен README

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,6 @@ import static com.github.javaparser.Providers.provider;
 
 public class Main {
     public static void main(String[] args) {
-        new JavaParser().parse(COMPILATION_UNIT, provider("class X{int a; }")).ifSuccessful(cu ->
-                System.out.println(cu)
-        );
+        new JavaParser().parse(COMPILATION_UNIT, provider("class X{int a; }")).ifSuccessful(System.out::println);
     }
 }
